e1a965776a4610ccf9b4d6184b8eda0ecc04df97
Tonny Mulli <tonnymulli2118@gmail.com>
2024-04-17 16:53:09 +0300

    acting on events

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+let dodger = document.getElementById('dodger');
+
+function moveDodgerLeft() {
+  let leftPosition = parseInt(dodger.style.left) || 0;
+  if (leftPosition > 0) {
+    dodger.style.left = `${leftPosition - 10}px`;
+  }
+}
+
+function moveDodgerRight() {
+  let leftPosition = parseInt(dodger.style.left) || 0;
+  if (leftPosition < 360) {
+    dodger.style.left = `${leftPosition + 10}px`;
+  }
+}
 // Your code here
